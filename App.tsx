9fda95d001a7a667653c43d07b3b17936ab6cfa7
itamar80 <itti37@gmail.com>
2023-11-30 12:46:14 +0200

    check push

--- App.tsx
+++ App.tsx
@@ -2,12 +2,8 @@ import React from 'react';
 import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
 } from 'react-native';
 
 type SectionProps = PropsWithChildren<{
